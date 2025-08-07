57753b36119225078b65b163294526654b6ca270
binizaa <binizacruuiz@gmail.com>
2025-08-07 13:12:27 -0600

    Initial Commit

---
+++ TicketBox/TicketBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketBoxApp.swift
+//  TicketBox
+//
+//  Created by Biniza Ruiz on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TicketBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicketBoxTests/TicketBoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicketBoxTests.swift
+//  TicketBoxTests
+//
+//  Created by Biniza Ruiz on 07/08/25.
+//
+
+import Testing
+@testable import TicketBox
+
+struct TicketBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
